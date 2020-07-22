0be3ea34988aebebe2748062cbe4ce14444c0356
Lukas Koncal <koncallukas@gmail.com>
2020-07-22 22:32:26 +0200

    Initial commit

---
+++ src/main/java/sk/konluk/AmbulanceDispatcher/AmbulanceDispatcherApplication.java
@@ -0,0 +1,13 @@
+package sk.konluk.AmbulanceDispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbulanceDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbulanceDispatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/konluk/AmbulanceDispatcher/AmbulanceDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.konluk.AmbulanceDispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbulanceDispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
